468d6389d488c6413aa6946bd5d9e5ce283f60fa
Mônica Colomera <colomeramonica@gmail.com>
2024-01-25 23:58:21 -0300

    fix: :recycle: Adicionando parametro self ao metodo

--- src/tech/domain/entities.py
+++ src/tech/domain/entities.py
@@ -1,19 +1,16 @@
 class TechCaseEntity:
 
-    def find_longest_word(input):
+    def find_longest_word(self, input):
 
         if not isinstance(input, str):
             raise TypeError("O input deve ser uma string.")
 
-        if not input.isalpha():
-            raise TypeError("O input só deve conter letras.")
-
         words = [input[i:i+n]
                  for i in range(len(input)) for n in range(2, len(input) - i + 1)]
 
-        biggest_word = max(words, key=len, default="")
+        longest_word = max(words, key=len, default="")
 
         singular_words = list(set(words))
         singular_words.sort(key=len, reverse=True)
 
-        return biggest_word, singular_words
+        return longest_word, singular_words
